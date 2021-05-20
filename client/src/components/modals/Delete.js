91d374633595c4d871c577ef724091fc14522408
Xavier Velez <xavier.velez@stonybrook.edu>
2021-05-20 18:28:51 -0400

    Adding jsTPS code, and delete modal not included in last commits

---
+++ client/src/components/modals/Delete.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { WModal, WMHeader, WMMain, WButton } from 'wt-frontend';
+
+const Delete = (props) => {
+
+    const handleDelete = async () => {
+        console.log(props.activeMap);
+        props.deleteMap(props.activeMap);
+        props.setShowDelete(false);
+    }
+
+    return (
+        <WModal className="delete-modal" cover="true" visible={props.setShowDelete}>
+            <WMHeader  className="modal-header" onClose={() => props.setShowDelete(false)}>
+                Delete Map?
+			</WMHeader >
+
+            <WMMain>
+                <WButton className="modal-button cancel-button" onClick={() => props.setShowDelete(false)} wType="texted">
+                    Cancel
+				</WButton>
+                <label className="col-spacer">&nbsp;</label>
+                <WButton className="modal-button" onClick={handleDelete} clickAnimation="ripple-light" hoverAnimation="darken" shape="rounded" color="danger">
+                    Delete
+				</WButton>
+            </WMMain>
+
+        </WModal >
+    );
+}
+
+export default Delete;
